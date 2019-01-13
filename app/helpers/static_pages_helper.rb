07a7eeff32f33e0499d8d93f12f29ff92b4f819e
rast-7 <rajatasthana4@gmail.com>
2019-01-13 13:07:28 +0530

    add albums

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,15 +1,21 @@
 module StaticPagesHelper
 	require 'httparty'
 
-	def search_api()		
+	def search_api		
 		url = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=#{@artist}&api_key=#{ENV['API_KEY']}&format=json"
 		response = HTTParty.get(url)
 		artist_info = JSON.parse(response.body)
 	end
 
-	def search_songs()
+	def search_songs
 		url = "https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=#{@artist}&limit=10&api_key=#{ENV['API_KEY']}&format=json"
 		response = HTTParty.get(url)
 		popular_songs = JSON.parse(response.body)
+	end
+
+	def search_albums
+		url = url = "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=#{@artist}&limit=10&api_key=#{ENV['API_KEY']}&format=json"
+		response = HTTParty.get(url)
+		popular_albums = JSON.parse(response.body)
 	end
 end
